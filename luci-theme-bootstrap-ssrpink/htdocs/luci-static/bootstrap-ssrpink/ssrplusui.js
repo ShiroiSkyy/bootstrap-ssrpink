2d6589a68316463faffb3e1202ac32ed71c305b4
ShiroiSky <shiroisky@163.com>
2020-03-05 00:17:47 +0800

    update and optimization

--- luci-theme-bootstrap-ssrpink/htdocs/luci-static/bootstrap-ssrpink/ssrplusui.js
+++ luci-theme-bootstrap-ssrpink/htdocs/luci-static/bootstrap-ssrpink/ssrplusui.js
@@ -1,4 +1,4 @@
-var Version=1.11;
+var Version=1.2;
 
 /* Cookie函数 */
 function setCookie(name,value){
@@ -9,77 +9,49 @@ function setCookie(name,value){
 }
 function getCookie(name){
     var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
-    if(arr=document.cookie.match(reg))
+    if(arr=document.cookie.match(reg)){
         return unescape(arr[2]);
-    else
+	}else{
         return null;
+	}
 }
-function delCookie(name){
-    var exp = new Date();
-    exp.setTime(exp.getTime() - 1);
-    var cval=getCookie(name);
-    if(cval!=null)
-        document.cookie= name + "="+cval+";expires="+exp.toGMTString();
-}
-/* removeClass函数 */
-function removeClass(href) {
-    var links = document.getElementsByTagName("link");
-    for (var i = 0; i < links.length; i++) {
-        var _href = links[i].href;
-        if (links[i] && links[i].href && links[i].href.indexOf(href) != -1) {
-            links[i].parentNode.removeChild(links[i]);
-        }
-    }
-}
-/* 初始载入主题 */
+/* 载入主题 */
 function loadStyle(url){
-	var ssrplusui = document.createElement('link');
-	ssrplusui.rel = 'stylesheet';
-	ssrplusui.type = 'text/css';
-	ssrplusui.href = url;
-	document.getElementsByTagName('head')[0].appendChild(ssrplusui);
+	var StyleSsr=document.querySelector("#ssrpinkcss");
+	if(StyleSsr!=null||""){
+		StyleSsr.href = url;
+	}else{
+		var ssrplusui = document.createElement('link');
+		ssrplusui.rel = 'stylesheet';
+		ssrplusui.type = 'text/css';
+		ssrplusui.id = 'ssrpinkcss';
+		ssrplusui.href = url;
+		document.getElementsByTagName('head')[0].appendChild(ssrplusui);
+	}
 }
 /* 切换主题函数 */
-function loadtheme(themeload){
-	switch(themeload){
-		case "dark":
-		case null:
-			themeload="light";
-			break;
-		case "light":
-			themeload="dark";
-			break;
-	}
-	removeClass(getCookie("themeurl"))
+function loadTheme(themeload){
 	loadStyle("/luci-static/bootstrap-ssrpink/ssrplusui-"+themeload+".css?v=shiroisky-v"+Version);
-	setCookie("themeurl","/luci-static/bootstrap-ssrpink/ssrplusui-"+themeload+".css?v=shiroisky-v"+Version);
 	setCookie("theme",themeload);
 }
 
 var str=window.location.href;
 var verification=new RegExp("/services/shadowsocksr");
 var verification_result=verification.test(str);
-var nowtheme=getCookie("theme");
+var oldtheme=getCookie("theme");
 if(verification_result==true){
-	if(nowtheme==null||nowtheme=="light"){
-		loadtheme("dark");
-	}else{
-		loadtheme("light");
-	}
+	//默认载入上次主题
+	loadTheme(oldtheme==null?"light":oldtheme=="light"?"light":"dark")
 	/* 切换主题 */
 	window.onload=function(){
-		var tabs=document.querySelector('.tabs');
 		var switch_theme=document.createElement('div');
 		switch_theme.classList.add('switch_theme');
 		var theme_text=document.createTextNode('切换');
 		switch_theme.appendChild(theme_text);
 		switch_theme.title="如果切换异常，请尝试清除浏览器Cookie！";
-		tabs.appendChild(switch_theme);
-		switch_theme=document.querySelector('.switch_theme');
-		switch_theme.onclick=function(){
-			loadtheme(getCookie("theme"));
+		document.querySelector('.tabs').appendChild(switch_theme);
+		document.querySelector('.switch_theme').onclick=function(){
+			loadTheme(getCookie("theme")=="dark"?"light":"dark");
 		}
 	}
-}else{
-	console.log("ShadowsocksR Plus+ UI is not loaded!");
 }
